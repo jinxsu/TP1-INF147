d1359e3fcfeaf969b5689bdf3487ea69e8f7b0e2
jinxsu <70604702+jinxsu@users.noreply.github.com>
2026-02-26 16:43:11 -0500

    adding documentation

---
+++ INF147-2_Samuel Dero ET Zhang Rui Chen_TP1/mtwister.h
@@ -0,0 +1,230 @@
+﻿#pragma once
+/*==========================================================*/
+
+/*
+	le module offrant un mersenne twister, un g�n�rateur de nombres
+	pseudo-al�atoires consid�r� comme le meilleur pour la simulation num�rique
+
+	Une courte pr�sentation francaise suit l'introduction en anglais des auteurs japonais
+	et donnera le caract�re g�n�ral de chaque  fonction offerte
+
+   Les 2 auteurs n'offraient originallement que des mtwister uniformes
+   qui peuvaient parfois paraitre redondantes. Pour un professionnel qui sait
+   construire des mtwister diff�rentes de l'uniforme, �a va
+   mais j'ai choisi d'ajouter pour les �tudiants quelques fonctions standardis�es
+   permettant d'obtenir des �v�nements dans une distribution :
+
+		uniforme g�n�rale , sans ou avec des bornes, enti�re ou r�elle
+		normale centr�e r�duite Z(0,1) ou normale � moyenne et �cart-type d�finis
+*/
+
+/*==========================================================*/
+/* Le module offre donc sept fonctions :
+
+	deux fonctions capables d'initialiser le g�n�rateur
+	1 - void mt_srand(unsigned long s);
+	2 - unsigned long mt_srandSys(void);
+
+	deux fonctions pour �mettre un nombre r�el dans une uniforme
+	double mt_rand(void);
+	double mt_randf(double b1, double b2);
+
+	une fonction pour �mettre un nombre entier dans une uniforme
+	int mt_randi(int borne);
+
+	deux fonctions pour �mettre un r�el dans une distribution normale
+	double mt_randz(void);
+	double mt_randnormal(double mu, double sigma);
+
+*/
+/*==========================================================*/
+
+
+/*==========================================================*/
+
+/*
+   A C-program for MT19937, with initialization improved 2002/1/26.
+   Coded by Takuji Nishimura and Makoto Matsumoto.
+
+   Before using, initialize the state by using init_genrand(seed)
+   or init_by_array(init_key, key_length).
+
+   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+	 1. Remtwister of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+
+	 2. Remtwister in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+
+	 3. The names of its contributors may not be used to endorse or promote
+		products derived from this software without specific prior written
+		permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+   Any feedback is very welcome.
+   http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+   email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
+*/
+
+/*==========================================================*/
+
+#ifndef _MT19937AR_H_
+#define _MT19937AR_H_
+/*==========================================================*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+
+/*==========================================================*/
+
+/* UNE INTERFACE MODULAIRE STANDARD POUR MES �TUDIANTS */
+
+/*==========================================================*/
+
+/*  Voici deux fonctions d'initialisation du
+	 g�n�rateur. Un g�n�rateur non-initialis� s'initialise
+	 en fait � 0 et �met la m�me s�quence ce qui fait douter
+	 du caract�re al�atoire.... C'est au client de l'initialiser
+	 MAIS JUSTE UNE FOIS dans tout le programme de simulation.
+*/
+
+/*==========================================================*/
+/* La fonction mt_srand
+ la forme d'initialisation la plus classique d'un g�n�rateur
+ � partir d'un entier quelconque, la s�quence pseudo al�atoire
+ � venir est totalement d�termin�e par la valeur re�ue
+
+PARAMETRE(s): s, un entier quelconque
+
+SORTIE: aucune
+
+SPECS: aucune
+*/
+void mt_srand(unsigned long s);
+
+/*==========================================================*/
+
+
+/*==========================================================*/
+/* la fonction mt_srandSys
+ une forme alternative d'initialisation du g�n�rateur � partir
+ de l'horloge interne de la machine,  l'horloge donne un entier
+ qui sert de param�tre � la fonction pr�c�dente.
+ Cette valeur est retourn�e
+
+PARAMETRE(s): Aucun
+
+SORTIE: l'entier qui a v�ritablement initialis� le g�n�rateur
+
+SPECS: aucune
+*/
+unsigned long mt_srandSys(void);
+/*==========================================================*/
+
+/* LES CINQ FONCTIONS D'�MISSION DANS LES mtwister SP�CIFI�ES */
+
+/*==========================================================*/
+/* La fonction mt_rand
+L'�mission d'un nombre r�el dans la distribution uniforme 0,1
+
+PARAMETRE(s): Aucun
+
+SORTIE : la valeur �mise par le g�n�rateur
+
+SPECS : aucune
+*/
+double mt_rand(void);
+/*==========================================================*/
+
+/*==========================================================*/
+/* La fonction mt_randf
+
+L'�mission d'un nombre r�el dans une  uniforme [b1, b2]
+
+PARAMETRE(s): deux valeurs r�elles quelconques qui serviront
+			   de bornes � l'intervalle
+
+SORTIE : la valeur �mise par le g�n�rateur
+
+SPECS : l'ordre des param�tres n'a aucune importance
+*/
+double mt_randf(double b1, double b2);
+/*==========================================================*/
+
+/*==========================================================*/
+/* La fonction mt_randi
+
+L'�mission d'un nombre entier positif dans une uniforme [1, borne]
+
+PARAMETRE(s): une valeur enti�re quelconque
+
+SORTIE : la valeur enti�re �mise par le g�n�rateur
+
+SPECS :  si le param�tre effectif < 0 , c'est la valeur absolue
+			du param�tre  qui sera consid�r�e
+		 recevant 0 elle retourne automatiquement 0 et
+			c'est le seul 0 possible
+*/
+int mt_randi(int borne);
+/*==========================================================*/
+
+/*==========================================================*/
+/* La fonction mt_randz
+
+L'�mission d'un nombre r�el dans la distribution normale Z,
+la plus classique  de moyenne 0 et d'�cart type 1
+
+PARAMETRE(s) : Aucun
+
+SORTIE : la valeur r�elle �mise par le g�n�rateur
+
+SPECS : aucune
+*/
+double mt_randz(void);
+/*==========================================================*/
+
+/*==========================================================*/
+/* La fonction mt_randnormal
+
+L'�mission d'un nombre r�el dans une distribution normale quelconque,
+  de moyenne mu  et d'�cart-type sigma donn�s
+
+PARAMETRE(s) : mu d�finit la moyenne voulue et sigma l'�cart-type
+
+SORTIE : la valeur r�elle �mise par le g�n�rateur
+
+SPECS : le signe de sigma n'aura pas d'importance.
+*/
+double mt_rand_normal(double mu, double sigma);
+
+/*==========================================================*/
+/*==========================================================*/
+
+/* L'INTERFACE ORIGINALE DES DEUX CR�ATEURS SERA DANS LE .C 	*/
+
+/*==========================================================*/
+#endif	
+/*==========================================================*/
+
+
--- INF147-2_Samuel Dero ET Zhang Rui Chen_TP1/t_pile.h
+++ INF147-2_Samuel Dero ET Zhang Rui Chen_TP1/t_pile.h
@@ -1,101 +1,70 @@
-/* T_PILE.C
+#pragma once
+/* T_PILE.H
    Module t_pile qui permettra d'implémenter des opérations de bases
    sur une pile d'entiers.
 
-   Auteur  : Zhang Rui Chen et Samuel Dero
+   Auteur  : Pierre Bélisle
+   Révision: Éric Thé
    Version : Copyright H2026
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include "t_pile.h"
+#ifndef T_PILE_H
+#define T_PILE_H
+
+typedef int t_element;   //modifier cette déclaration pour avoir une pile d'un autre type
+
+/* le type-enregistrement pour une pile */
+typedef struct {
+	t_element* items;	//tableau dynamique des éléments de la pile
+	int sommet;			//position du sommet (top) de la pile. sera == -1 lorsque vide 
+	int taille;			//taille maximale du tableau dynamique
+} t_pile;
+
+
+/* Voici le constructeur: */
 
 /* Le constructeur reçoit une capacité maximale d'éléments "taille"
 retourne une pile initialisée à vide de cette taille. */
-t_pile init_pile(unsigned int taille)
-{
-	return (t_pile) {(int*)malloc(taille*sizeof(int)), -1, taille };
-	
-}
+t_pile init_pile(unsigned int taille);
+
+
+/* Voici le destructeur: */
 
 /* Fonction-destructeur qui va libérer le tableau dynamique de la pile.
 Cette pile ne devra plus être utilisé avant d’avoir été réinitialisée. */
-void liberer_pile(t_pile* pile)
-{
-	free(pile->items);
-	pile->items = NULL;
-	pile->sommet = -1;
-	pile->taille = 0;
-	
-}
+void liberer_pile(t_pile* pile);
+
+
+/* Voici quatre fonctions informatrices de l’état de la pile : */
 
 /* Obtenir le nombre d'éléments actuellement dans la pile reçue en paramètre. */
-unsigned int get_nb_elements(const t_pile* pile)
-{
-	return pile->sommet + 1;
-}
+unsigned int get_nb_elements(const t_pile* pile);
 
 /* Obtenir la capacité maximale de la pile reçue en paramètre. */
-unsigned int get_max_pile(const t_pile* pile)
-{
-	return pile->taille;
-}
+unsigned int get_max_pile(const t_pile* pile);
 
 /* Obtenir une valeur actuellement dans la pile.
 On reçoit trois paramètre : le pointeur d'une pile,
 la position voulue (elle doit être valide, de 0 à p->sommet)
 et la référence ou copier l'élément à la position reçue.
 On retourne 1 si la position reçue est valide, et 0 sinon. */
-int get_element(const t_pile* pile, unsigned int position, t_element* elem)
-{
-	if(position > pile->sommet || position < 0) {
-		return 0;
-	}
-	*elem = pile->items[position];
-	return 1;
-		
-	
-}
+int get_element(const t_pile* pile, unsigned int position, t_element* elem);
 
 /* Confirme la présence de la valeur "elem" dans la pile.
 On retourne 0 si la valeur "elem" n'y est pas et 1 si elle est présente. */
-int present_pile(const t_pile* pile, t_element elem)
-{
-	for(int i=0; i < pile->sommet; i++) {
-		if(pile->items[i] == elem) {
-			return 1;
-		}
-		
-	}
-	return 0;
-}
+int present_pile(const t_pile* pile, t_element elem);
+
+
+/* Voici les deux fonctions mutatrices de l’état de la pile : */
 
 /* Extraction de la pile (pop):
 On reçoit le pointeur d'une pile existante et la référence ou copier l'élément extrait.
 Retour de 0 si la pile est vide -- seul cas possible. Sinon 1 (extraction réussie). */
-int pop_pile(t_pile* p, t_element* dest)
-{
-	if(p->sommet == -1) {
-		return 0;
-	}
-	*dest = p->items[p->sommet];
-	p->sommet--;
-	return 1;
-	
-}
+int pop_pile(t_pile* p, t_element* dest);
 
 /* Insertion dans la pile (push):
 On reçoit le pointeur d'une pile et la valeur "src" à insérer.
 Retour de 0 si la pile est pleine -- seul cas possible. Sinon 1 (insertion réussie). */
-int push_pile(t_pile* p, t_element src)
-{
-	if(p->sommet==p->taille-1) {
-		return 0;
-	}
-	p->sommet++;
-	p->items[p->sommet] = src;
-	return 1;	
-	
-}
+int push_pile(t_pile* p, t_element src);
 
-
+#endif // T_PILE__
 
